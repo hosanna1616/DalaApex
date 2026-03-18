c4d1a01f03f67f84a6c247f828dcd7f7693e8bcb
hosanna1616 <hosannawalle@gmail.com>
2026-03-18 21:30:46 +0300

    Add context files

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react-refresh/only-export-components */
+import type { FC, ReactNode } from "react";
+import { createContext, useContext, useState } from "react";
+import { translations } from "../translations";
+
+type Language = "en" | "am";
+
+interface LanguageContextType {
+  language: Language;
+  toggleLanguage: () => void;
+  t: typeof translations.en;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined,
+);
+
+export const LanguageProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [language, setLanguage] = useState<Language>("en");
+
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "en" ? "am" : "en"));
+  };
+
+  const t = translations[language];
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage, t }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+  return context;
+};
